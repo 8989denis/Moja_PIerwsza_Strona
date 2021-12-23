04f3566ee13580ed11c8ddb6562c773116f8a0ec
Denis <30268500+8989denis@users.noreply.github.com>
2021-12-23 17:35:35 +0100

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,2 @@
+alert("Uwaga wchodzisz na strone denis82.com");
+alert("Uwaga jeśli zgadzasz wejsc na strone denis82.com naciśnij ponownie 'ok'");
---
+++ przycisk.js
@@ -0,0 +1,11 @@
+const przycisk=document.getElementById="button1";
+
+const kontener=document.getElementById="kontener";
+
+przycisk.addEventListener("click", pierwszafunkcja);
+
+function pierwszafunkcja() {
+    
+ kontener.textContent = "zadziałało" ;
+    
+}
